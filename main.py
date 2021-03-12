5c516a4112173731c1b2c70c836250d682b67e17
ebochat <73542123+rbardinet@users.noreply.github.com>
2021-03-12 18:13:34 +0100

    Ajouté affichage pieces fonction glouton

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 ﻿euroLib = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8"]
 euroTab = [200, 100, 50, 20, 10, 5, 2, 1]
 
+def listCounter(listC):
+    varList = [[],[]]
+    #varList[0] contient les pieces cX
+    #varList[1] contient le nombre de fois que chaque vpiece apparait
+    for k in range (len(listC)):
+        if (listC[k] not in varList[0]):
+            varList[0].append(listC[k])
+            varList[1].append(1)
+        else :
+            i = 0
+            flag = 0
+            while (i < len(varList[0]) and flag!=1):
+                
+                if (varList[0][i]==listC[k]):
+                    varList[1][i]+=1
+                    flag = 1
+                i+=1
+    #Affichage 
+    for k in range (len(varList[0])) :
+        print("La piece {0} apparait {1} fois".format(varList[0][k],varList[1][k]))
+    
+
+        
 
 def glouton(aRendre):
     pieces =  []
@@ -13,8 +36,9 @@ def glouton(aRendre):
                 searching = False
                 aRendre -= euroTab[i]
             i += 1
+    print(pieces)
     return pieces
 
 
 if __name__ == "__main__":
-    print(glouton(138))
+    listCounter(glouton(4589))
