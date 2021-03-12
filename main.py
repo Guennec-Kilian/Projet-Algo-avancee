c1736d3abf5cf6133e5606f102c22f66bbc2371e
kilian <kguennec@enssat.fr>
2021-03-12 17:41:25 +0100

    algo glouton fini

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 ﻿euroLib = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8"]
-euroTab = [2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
+euroTab = [200, 100, 50, 20, 10, 5, 2, 1]
 
 
 def glouton(aRendre):
@@ -13,8 +13,8 @@ def glouton(aRendre):
                 searching = False
                 aRendre -= euroTab[i]
             i += 1
-        
+    return pieces
 
 
 if __name__ == "__main__":
-    print(glouton(12.38))
+    print(glouton(138))
