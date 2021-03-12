d2fa5a23b885f8ab9cbbb6962f839b25ecc28186
kilian <kguennec@enssat.fr>
2021-03-12 21:16:58 +0100

    ajout d'une classe Arbre

---
+++ Arbre.py
@@ -0,0 +1,60 @@
+﻿from copy import deepcopy
+euroLib = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8"]
+euroTab = [200, 100, 50, 20, 10, 5, 2, 1]
+
+class Arbre:
+    """
+        implémentation des arbres pour pouvoir chercher la solution optimal
+        via les essais successifs
+    """
+    feuilles = []
+    valeur = 0
+    chemin = []
+
+    def __init__(self, v_parent, chemin_parent, indicePiece):
+        """
+            Si indicePiece == -1, il s'agit d'une initialisation de l'arbre et non pas d'une feuille
+        """
+        if indicePiece != -1:
+            self.valeur = v_parent + euroTab[indicePiece]
+            self.chemin = chemin_parent
+            self.chemin.append(euroLib[indicePiece])
+            self.feuilles = []
+
+    def addFeuille(self, indicePiece):
+        self.feuilles.append(Arbre(self.valeur, deepcopy(self.chemin), indicePiece))
+        
+    def getSortedChemin(self):
+        """
+         pour pouvoir ensuite passer par une fonction d'affichage
+        """
+        s_chemin = deepcopy(self.chemin)
+        s_chemin.sort()
+        return s_chemin
+
+    def isLower(self, limite):
+        return self.valeur < limite
+
+    def isEqual(self, limite):
+        return self.valeur == limite
+
+    def getLenChemin(self):
+        return len(self.chemin)
+
+    def affichage(self):
+        subA = [f.affichage() for f in self.feuilles if self.feuilles != []]
+        res = "<{0}, ".format(self.valeur)
+        if subA != []:
+            for a in subA:
+                res += "{0}, ".format(a)
+            return res[:-2] + ">"
+        else:
+             return res + ">"
+
+if __name__ == "__main__":
+    a = Arbre(0, [], -1)
+    for k in range (len(euroLib)):
+        a.addFeuille(k)
+    a.feuilles[-1].addFeuille(3)
+
+    print(a.affichage())
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-﻿euroLib = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8"]
+﻿import Arbre
+
+A = Arbre.Arbre
+euroLib = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8"]
 euroTab = [200, 100, 50, 20, 10, 5, 2, 1]
+
 
 def listCounter(listC):
     varList = [[],[]]
@@ -21,24 +25,40 @@ def listCounter(listC):
     #Affichage 
     for k in range (len(varList[0])) :
         print("La piece {0} apparait {1} fois".format(varList[0][k],varList[1][k]))
-    
 
+def essaiSucc(a, N, solution):
+    # a le noeud que l'on traite
+    # N la valeur a atteindre
+    for k in range (len(euroLib)):
+        a.addFeuille(k)
+    
+    for x in a.feuilles:
+        if x.isEqual(N):
+            if len(solution) == 0 or len(solution) > x.getLenChemin():
+                solution = x.getSortedChemin()
+        elif x.isLower(N):
+            essaiSucc(x, N, solution)
         
 
 def glouton(aRendre):
     pieces =  []
+    troubleshout = 0
     while aRendre != 0:
         searching = True
         i = 0
         while searching and i < len(euroLib):
             if aRendre >= euroTab[i]:
+                # si le reste à rendre est > a la valeur de la pièce
+                # (on suppose les pièces ordonnées par valeurs décroissantes)
                 pieces.append(euroLib[i])
                 searching = False
                 aRendre -= euroTab[i]
             i += 1
-    print(pieces)
+        troubleshout += 1
+        if troubleshout == 100000:
+            raise TimeoutError('nombre de recherche trop important')
+
     return pieces
 
-
 if __name__ == "__main__":
-    listCounter(glouton(4589))
+    listCounter(glouton(1))
